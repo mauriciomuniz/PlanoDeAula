70af60ac46af4634e6d8cce5faa985a299a47bb8
Mauricio <mauricio.santana2010@hotmail.com>
2025-01-04 03:05:24 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/PlanoDeAula.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlanoDeAula.Communication.Requests;
+using PlanoDeAula.Communication.Responses;
+
+namespace PlanoDeAula.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
